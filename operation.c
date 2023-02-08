2981de4e01fe31a8223e056bac185a59c88f129c
Lucia Jimena Martin Rodriguez <lucia-ma@c1r1s3.42madrid.com>
2023-02-08 16:59:04 +0100

    push x y idea

--- operation.c
+++ operation.c
@@ -8,6 +8,16 @@ typedef struct s_doublelist {
 }   t_doublelist;
 
 
+void    ft_printlista(t_doublelist *lista)
+{
+    while(lista->next)
+    {
+        printf("content %d\n", lista->content);
+        lista = lista->next;
+    }
+    printf("content %d\n", lista->content);
+}
+
 void    *doublylink_new(int content, t_doublelist **prevstack, int orden)
 {
     t_doublelist *new;
@@ -43,20 +53,49 @@ void    swap_a_and_b(t_doublelist **stack_a, t_doublelist **stack_b)
     (* stack_a)->content = (*stack_a)->next->content;
     (*stack_a)->next->content = swap;
 
-    swap = (*stack_b)->content;
-    (*stack_b)->content = (*stack_b)->next->content;
-    (*stack_b)->next->content = swap;
+    if((*stack_b)->content)
+    {
+        swap = (*stack_b)->content;
+        (*stack_b)->content = (*stack_b)->next->content;
+        printf("donde fallas bebe?\n"); 
+        (*stack_b)->next->content = swap;
+    }
 }
-/* 
-int    *push_x(int *stack_x, int *stack_y)
+ 
+void    push_x(t_doublelist **stack_b, t_doublelist **stack_a)
 {
-    int *stack_ny;
-//no se muy bien si en y hay que rota y luego meter el primero del b
-//creo que lo mejor es crear otro stack reservar memoria para leng de y 
-//+1 meter numero de x y todo y && crear stack x y meter todo x
+    t_doublelist    *swap;
+
+    swap = *stack_a;
+
+    *stack_a = (*stack_a)->next;
+    (*stack_a)->prev = NULL;
+    printf("PRINTEAMOS LISTA STACK_A \n");
+    ft_printlista(*stack_a);
+
+    swap->next = *stack_b;
+    printf("PRINTEAMOS LISTA SWAP \n");
+    ft_printlista(swap);
+
+    *stack_b = swap;
+     printf("PRINTEAMOS LISTA STACK_B \n");
+    ft_printlista(*stack_b);
+
+    
+
+
+    // t_doublelist    *swap;
+
+    // swap = *stack_a;
+    // (*stack_a)->next->prev = NULL;
+    // *stack_a = (*stack_a)->next;
+    // printf("contenido de a == %d\n", swap->content);
+    // (*stack_b)->prev = swap;
+    // (*stack_b) = (*stack_b)->prev;
+
 }
 
-
+/*
 void rotate_a_or_b(int *stack)
 {
     int init;
@@ -71,34 +110,21 @@ void rotate_a_or_b(int *stack)
     }
 }*/
 
-t_doublelist *build_lst( char **argv, int argc )
+
+
+t_doublelist *build_lst( char **argv, int argc)
 {
-    t_doublelist *head_a;
-    t_doublelist *init_a;
+    // t_doublelist *head_a;
+    // t_doublelist *init_a;
     
-    head_a = doublylink_new(atoi(argv[0]), NULL, 0);
-    init_a = head_a;
-    int i = 1;
-    argc = argc - 2;
-    while(argc >= i++)
-    {
-        printf("holi %d\n", i);
-        printf("guatafac\n");
-        doublylink_new(atoi(argv[i]), &head_a, 1);
-    }
-    printf("juju\n");
-    return( init_a );
+    // head_a = doublylink_new(atoi(argv[0]), NULL, 0);
+    // init_a = head_a;
+    // int i = 1;
+    // while(i < argc)
+    //     doublylink_new(atoi(argv[i++]), &head_a, 1);
+    // return( init_a );
 }
 
-void    ft_printlista(t_doublelist *lista)
-{
-    while(lista->next)
-    {
-        printf("content %d\n", lista->content);
-        lista = lista->next;
-    }
-    printf("content %d\n", lista->content);
-}
 
 int main(int argc, char **argv)
 {
@@ -109,14 +135,18 @@ int main(int argc, char **argv)
   
     if (argc == 1)
         return(0);
-    head_b = build_lst(argv, argc);
-    head_a = build_lst(++argv, argc);
-    printf("buenop\n");
+    head_b = malloc(sizeof(t_doublelist *));
+    head_a = build_lst(++argv, argc - 1);
     init_a = head_a;
     init_b = head_b;
     ft_printlista(init_a);
-    printf("hola    \n\n");
-    swap_a_and_b(&head_a, &head_b);
-    ft_printlista(head_a);
+    printf("\n\n");
+    push_x(&init_b, &init_a);
+    printf("\n\ninit_b\n");
+    ft_printlista(init_b);
+    printf("\n\ninit_a\n");
+    ft_printlista(init_a);
     return(0);
 }
+
+
--- t.c
+++ t.c
@@ -2,17 +2,48 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct s_doublelist {
+    int content;
+    struct s_doublelist *next;
+    struct s_doublelist *prev;
+}   t_doublelist;
+
+void    ft_printlista(t_doublelist *lista)
+{
+    while(lista->next)
+    {
+        printf("content %d\n", lista->content);
+        lista = lista->next;
+    }
+    printf("content %d\n", lista->content);
+}
+
+void build_list(t_doublelist **stack, char **argv, int argc)
+{
+    t_doublelist    *swap;
+    t_doublelist    *init;
+    int             i;
+
+    i = 0;
+    init = *stack;
+    while(i < argc)
+    {
+        swap->prev = *stack;
+        swap->next = NULL;
+        swap->content = atoi(argv[i]);
+        i++;
+        (*stack)->next= swap;
+        *stack = (*stack)->next;
+    }   
+    ft_printlista(init);
+    *stack = init;
+}
+
 int main(int argc, char **argv)
 {
-    while(*argv)
-    {
-        while(**argv)
-        {
-            printf("jujiju  %c\n", **argv);
-            (*argv) ++;
-        }
-        argv++;
-    }
-    
-    return (0);
+    t_doublelist    *stack;
+
+    stack =  malloc(sizeof(t_doublelist *));
+    build_list(&stack, argv, argc);
+    ft_printlista(stack);
 }
