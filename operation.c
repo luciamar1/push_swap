c740e400a68a678a0034a4977d3232b3131f1308
Lucia Jimena Martin Rodriguez <lucia-ma@c3r14s5.42madrid.com>
2023-03-02 18:23:03 +0100

    me he cargado la creacion de listas

--- operation.c
+++ operation.c
@@ -18,20 +18,32 @@ void    ft_printlista(t_doublelist *lista)
     printf("content %d\n", lista->content);
 }
 
-void    *doublylink_new(int content, t_doublelist **prevstack, int orden)
+void    ft_freedoublelist(t_doublelist *lista)
+{
+    lista = lista->next;
+    while(lista->next)
+    {
+        free(lista->prev);
+        lista = lista->next;
+    }
+}
+
+void    *doublylink_new(int content, t_doublelist *prevstack, int orden)
 {
     t_doublelist *new;
     new = malloc(sizeof(t_doublelist *));
     if (new == NULL)
-        return(0);
+        return(NULL);
     new->content = content;
     new->next =  NULL;
+    printf("plis%d\n", new->content);
     if(orden == 1)
     {
-        new->prev = *prevstack;
-        *prevstack = new;
-        (*prevstack)->prev->next = new;
-        return(0);
+        new->prev = prevstack;
+        prevstack = new;
+        prevstack->prev->next = new;
+        printf("        2       plis%d\n", new->content);
+        return("bien");
     }
     new->prev = NULL;
     return(new);
@@ -62,37 +74,21 @@ void    swap_a_and_b(t_doublelist **stack_a, t_doublelist **stack_b)
     }
 }
  
-void    push_x(t_doublelist **stack_b, t_doublelist **stack_a)
+void    push_x(t_doublelist *stack_b, t_doublelist *stack_a)
 {
     t_doublelist    *swap;
 
-    swap = *stack_a;
-
-    *stack_a = (*stack_a)->next;
-    (*stack_a)->prev = NULL;
-    printf("PRINTEAMOS LISTA STACK_A \n");
-    ft_printlista(*stack_a);
-
-    swap->next = *stack_b;
+    swap = stack_a;
+    stack_a = (stack_a)->next;
+    (stack_a)->prev = NULL;
+    swap->next = stack_b;
+    stack_b = swap;
     printf("PRINTEAMOS LISTA SWAP \n");
     ft_printlista(swap);
-
-    *stack_b = swap;
-     printf("PRINTEAMOS LISTA STACK_B \n");
-    ft_printlista(*stack_b);
-
-    
-
-
-    // t_doublelist    *swap;
-
-    // swap = *stack_a;
-    // (*stack_a)->next->prev = NULL;
-    // *stack_a = (*stack_a)->next;
-    // printf("contenido de a == %d\n", swap->content);
-    // (*stack_b)->prev = swap;
-    // (*stack_b) = (*stack_b)->prev;
-
+    printf("PRINTEAMOS LISTA STACK_A \n");
+    ft_printlista(stack_a);
+    printf("PRINTEAMOS LISTA STACK_B \n");
+    ft_printlista(stack_b);
 }
 
 /*
@@ -114,15 +110,30 @@ void rotate_a_or_b(int *stack)
 
 t_doublelist *build_lst( char **argv, int argc)
 {
-    // t_doublelist *head_a;
-    // t_doublelist *init_a;
+    t_doublelist    *head_a;
+    t_doublelist    *init_a;
     
-    // head_a = doublylink_new(atoi(argv[0]), NULL, 0);
-    // init_a = head_a;
-    // int i = 1;
-    // while(i < argc)
-    //     doublylink_new(atoi(argv[i++]), &head_a, 1);
-    // return( init_a );
+    head_a = doublylink_new(atoi(argv[0]), NULL, 0);
+    if(head_a == NULL)
+        return (NULL);
+    init_a = head_a;
+    printf("INIT_A =)\n");
+    ft_printlista(init_a);
+    printf("INIT_A =)\n\n");
+    int i = 1;
+    while(i < argc)
+    {
+        if (doublylink_new(atoi(argv[i++]), head_a, 1) == NULL)
+        {
+            printf("sdsojjdjojds\n");
+            ft_freedoublelist(head_a);
+            return(NULL);
+        }
+    }
+    printf("INIT_A =)\n");
+    ft_printlista(init_a);
+    printf("INIT_A =)\n\n");
+    return( init_a );
 }
 
 
@@ -134,18 +145,19 @@ int main(int argc, char **argv)
     t_doublelist *head_b;
   
     if (argc == 1)
-        return(0);
-    head_b = malloc(sizeof(t_doublelist *));
+        return(1);
+    // head_b = malloc(sizeof(t_doublelist *));
+    // if(head_b == NULL)
+    //     return (0);
     head_a = build_lst(++argv, argc - 1);
+    if (head_a == NULL)
+        return (1);
     init_a = head_a;
     init_b = head_b;
+    printf("init_a\n");
     ft_printlista(init_a);
     printf("\n\n");
-    push_x(&init_b, &init_a);
-    printf("\n\ninit_b\n");
-    ft_printlista(init_b);
-    printf("\n\ninit_a\n");
-    ft_printlista(init_a);
+    push_x(init_b, init_a);
     return(0);
 }
 
