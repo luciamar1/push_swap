a65133d5c7052ae184f97c2861643eb65d1d5e9e
Lucia Jimena Martin Rodriguez <lucia-ma@c2r5s2.42madrid.com>
2023-03-09 13:25:45 +0100

    pr

--- operation.c
+++ operation.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:19:38 by lucia-ma          #+#    #+#             */
-/*   Updated: 2023/03/07 19:35:15 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/03/09 13:24:47 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,13 @@ typedef struct s_doublelist {
 	struct s_doublelist	*prev;
 }	t_doublelist;
 
-void	ft_printlista(t_doublelist *lista)
+void	ft_printlista(t_doublelist *lista, char x)
 {
 	while (lista->next)
 	{
-		printf("content %d\n", lista->content);
+		printf("printlista %c contenido == %d\n", x, lista->content);
 		lista = lista->next;
 	}
-	printf("content %d\n", lista->content);
 }
 
 void	ft_freedoublelist(t_doublelist *lista)
@@ -50,15 +49,9 @@ void	*doublylink_new(int content, t_doublelist **prevstack, int orden)
 	new->next = NULL;
 	if (orden == 1)
 	{
-		printf("\n\n YUHHHHHHUUUUUUUU");
 		new->prev = *prevstack;
 		*prevstack = new;
 		(*prevstack)->prev->next = new;
-		// printf("      DOUBLYLYNK\n");
-		// ft_printlista((*prevstack)->prev);
-		// printf("memeto??%d\n", prevstack->prev->next->content);
-		printf("YUHHHHHHUUUUUUUU\n\n");
-		ft_printlista(new->prev);
 		return ("bien");
 	}
 	new->prev = NULL;
@@ -85,7 +78,6 @@ void	swap_a_and_b(t_doublelist **stack_a, t_doublelist **stack_b)
 	{
 		swap = (*stack_b)->content;
 		(*stack_b)->content = (*stack_b)->next->content;
-		printf("donde fallas bebe?\n"); 
 		(*stack_b)->next->content = swap;
 	}
 }
@@ -93,21 +85,12 @@ void	swap_a_and_b(t_doublelist **stack_a, t_doublelist **stack_b)
 void	push_x(t_doublelist **stack_b, t_doublelist **stack_a)
 {
 	t_doublelist	*swap;
-
+	
 	swap = *stack_a;
 	*stack_a = (*stack_a)->next;
-	printf("\n\nhollliii %d\n", (*stack_a)->content);
-	ft_printlista(*stack_a);
-	printf("\n\nhollliii\n");
 	(*stack_a)->prev = NULL;
 	swap->next = *stack_b;
 	*stack_b = swap;
-	printf("PRINTEAMOS LISTA SWAP \n");
-	ft_printlista(swap);
-	printf("PRINTEAMOS LISTA *STACK_A \n");
-	ft_printlista(*stack_a);
-	printf("PRINTEAMOS LISTA *STACK_B \n");
-	ft_printlista(*stack_b);
 }
 
 /*
@@ -144,6 +127,7 @@ t_doublelist	*build_lst(char **argv, int argc)
 			return (NULL);
 		}
 	}
+	ft_printlista(init_a, 'A');
 	return (init_a);
 }
 
@@ -156,19 +140,12 @@ int	main(int argc, char **argv)
 
 	if (argc == 1)
 		return (1);
-	// head_b = malloc(sizeof(t_doublelist *));
-	// if(head_b == NULL)
-	//     return (0);
 	head_a = build_lst(++argv, argc - 1);
 	if (head_a == NULL)
 		return (1);
 	init_a = head_a;
-	init_b = head_b;
+	//init_b = head_b;
 	push_x(&init_b, &init_a);
-	printf("init_a\n");
-	ft_printlista(init_a);
-	printf("\n\n");
-	printf("init_b\n");
-	ft_printlista(init_b);
+	ft_printlista(init_b, 'B');
 	return (0);
 }
