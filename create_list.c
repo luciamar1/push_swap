6b81798aa37cb8d4116b380d3c00df1e0ca00974
Lucia Jimena Martin Rodriguez <lucia-ma@c1r5s5.42madrid.com>
2023-03-27 17:47:12 +0200

    arreglar stacka y cambiar por ende los movimientos

--- create_list.c
+++ create_list.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 17:13:37 by lucia-ma          #+#    #+#             */
-/*   Updated: 2023/03/24 20:24:12 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/03/27 17:45:05 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,54 +58,66 @@ int ft_create_dlist(t_dlist **saltarina, int data)
     }
     //printf("array %d\n", (*saltarina)->content);
     *saltarina = new ;
-    printf("ooooyyyyyyeeee %d\n", (*saltarina)->content);
     return(0);
 }
 
-char *ft_reverse_string(char *str)
+int swap_a_or_b(t_dlist  **stack)
 {
-    int swap;
-    int len;
-    int i;
-    int impar;
     
-    len = ft_strlen(str);
-    if(len % 2 != 0)
-    {
-        swap = len / 2;
-        impar = 1;
-    }
-    else 
-        impar = 0;
-    i = 0;
-    while(len)
-    {
-        if(impar == 1 && len-- == swap)
-            str[i] = str[swap];        
-        else
-            str[i++] = str[len--];
-    }
-    return(str);
+    int     swap;
+    
+    if(!*stack)
+        return (1);
+    swap = (*stack)->content;
+    (*stack)->content = ((*stack)->prev->content);
+    (*stack)->prev->content = swap;
+    return(0);
 }
+
+void    swap_a_and_b(t_dlist **stack_a, t_dlist **stack_b)
+{
+    swap_a_or_b(stack_a);
+    swap_a_or_b(stack_b);
+}
+
+void    push_x(t_dlist **stack_b, t_dlist **stack_a)
+{
+        t_dlist    *swap;
+
+        swap = *stack_a;
+        *stack_a = (*stack_a)->next;
+        (*stack_a)->prev = NULL;
+
+        swap->next = *stack_b;
+        *stack_b = swap;
+}
+
+
 
 int main(int argc, char **argv)
 {
     t_dlist *stack_a;
+    t_dlist *stack_b;
     int     i;
     int ar = argc;
+    t_dlist *f_stack_a;
     i = ar;
     i = 1;
     stack_a = NULL;
-    
+    ft_create_dlist(&stack_a, ft_atoi(argv[i++]));
+    f_stack_a = stack_a;
+
     while(argv[i])
     {
-        argv[i] = ft_reverse_string(argv[i]);
-        printf("string reverse%s\n", argv[i]);
         ft_create_dlist(&stack_a, ft_atoi(argv[i++]));
     }
-    printf("%d\n", stack_a->content);
-    printf("%d\n", stack_a->next->content);
-    printf("%d\n", stack_a->next->next->content);    
+    
+    //swap_a_or_b(&f_stack_a);
+    //swap_a_and_b(&f_stack_a, &stack_b);
+
+    printf("%d\n", f_stack_a->content);
+    printf("%d\n", f_stack_a->prev->content);
+    printf("%d\n", f_stack_a->prev->prev->content);    
     //ft_print_dlist(stack_a);
     return (0);
 }
