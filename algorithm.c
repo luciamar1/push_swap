8a10734405967d73ba4e034af04e3d41b88f5667
Lucia Jimena Martin Rodriguez <lucia-ma@c1r5s2.42madrid.com>
2023-04-14 22:44:20 +0200

    push chachi

--- algorithm.c
+++ algorithm.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 20:20:21 by lucia-ma          #+#    #+#             */
-/*   Updated: 2023/04/13 15:32:23 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/04/14 18:30:29 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,23 @@ void    push_20(t_dlist **stack_a, t_dlist **stack_b, int max)
     int len;
     
     len = ft_len_dlist(*stack_a);
-    printf("===== %d\n", len);
+    // printf("===== %d\n", len);
     while(len --)
     {
-        // printf("max %d     content %d\n", max, (*stack_a)->content);
+        //printf("max %d     content %d\n", max, (*stack_a)->content);
         if((*stack_a)->content <= max)
         {
-            printf("HA ENCONTRADO LIMITE %d\n", (*stack_a)->content);
+            // printf("HA ENCONTRADO LIMITE %d\n", (*stack_a)->content);
             push_x(&(*stack_b), &(*stack_a));
-            printf("GUATATATAAAAAA %d\n", (*stack_a)->content);
-            printf("GUATATATAAAAAA NEXT %d\n", (*stack_a)->next->next->content);
-            printf("GUATATATAAAAAA PREV %d\n", (*stack_a)->prev->content);
+            // printf("GUATATATAAAAAA %d\n", (*stack_a)->content);
+            // printf("GUATATATAAAAAA NEXT %d\n", (*stack_a)->next->next->content);
+            // printf("GUATATATAAAAAA PREV %d\n", (*stack_a)->prev->content);
         }
         else
             *stack_a = (*stack_a)->next;
-       // printf("while push %d\n", (*stack_a)->content);
+       //printf("while push %d\n", (*stack_a)->content);
     }
+    printf("stack_a == %d\n", (*stack_a)->content);
 }
 
 void    order_nums(t_dlist **stack_b, t_dlist **stack_a)
@@ -60,7 +61,7 @@ void    order_nums(t_dlist **stack_b, t_dlist **stack_a)
     limit = 20;
     while(limit && (*stack_b)->next != (*stack_b)->next->next)
     {
-        printf("while order_nums\n");
+        // printf("while order_nums\n");
         if((*stack_b)->content > max->content)
             max = *stack_b;
         (*stack_b) = (*stack_b)->next;
@@ -77,13 +78,13 @@ void algorithm(t_dlist **stack_a, t_dlist **stack_b)
     limit = 20;
     while((*stack_a))
     {
-        printf("CARAPAAAAAAAAAN    limit ====  %d\n", limit);
+        // printf("CARAPAAAAAAAAAN    limit ====  %d\n", limit);
         push_20(stack_a, stack_b, limit);
         limit += 20;
     }
     while((*stack_b)->next != (*stack_b)->next->next)
     {
-        printf("while algorithm 1\n");
+        // printf("while algorithm 1\n");
         order_nums(stack_b, stack_a);
     }
     push_x(stack_b, stack_a);
--- movements.c
+++ movements.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 12:27:02 by lucia-ma          #+#    #+#             */
-/*   Updated: 2023/04/13 19:37:27 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/04/14 18:21:18 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,32 +47,48 @@ int swap_a_or_b(t_dlist  **stack)
     return(0);
 }
 
-void    push_x(t_dlist **stack_b, t_dlist **stack_a)
-{
-    t_dlist    *swap;
+// void    push_x(t_dlist **stack_b, t_dlist **stack_a)
+// {
+//     t_dlist    *swap;
 
-    swap = *stack_a;
-    *stack_a = (*stack_a)->next;
-    (*stack_a)->prev = swap->prev;
-    swap->next = *stack_b;
-    if(stack_b)
-    {
-        swap->prev = (*stack_b)->prev;
-        (*stack_b)->prev = swap;
-    }
-    printf("guatatatatattatatatatatatatatatatatata     =====    \n");
-    *stack_b = swap;
-    (*stack_b)->next = *stack_b;
-    if(*stack_a == swap)
-        *stack_a = NULL;
-}
+//     swap = *stack_a;
+//     *stack_a = (*stack_a)->next;
+//     (*stack_a)->prev = swap->prev;
+//     swap->next = *stack_b;
+//     if(stack_b)
+//     {
+//         swap->prev = (*stack_b)->prev;
+//         (*stack_b)->prev = swap;
+//     }
+//     printf("guatatatatattatatatatatatatatatatatata     =====    \n");
+//     *stack_b = swap;
+//     (*stack_b)->next = *stack_b;
+//     if(*stack_a == swap)
+//         *stack_a = NULL;
+// }
 
 void    push_x(t_dlist **stack_b, t_dlist **stack_a)
 {
     t_dlist   *swap;
 
     swap = *stack_a;
-    
+    *stack_a =  (*stack_a)->next;
+    (*stack_a)->prev = swap->prev;
+    swap->prev->next = *stack_a;
+    if(*stack_b)
+    {
+        swap->prev = (*stack_b)->prev;
+        swap->next = (*stack_b)->next;
+        (*stack_b)->prev->next = swap;
+        (*stack_b)->prev = swap;
+        *stack_b = swap;
+    }
+    else
+    {
+        *stack_b = swap;
+        (*stack_b)->next = (*stack_b);
+        (*stack_b)->prev = (*stack_b);
+    }
 }
 
 void    swap_a_and_b(t_dlist **stack_a, t_dlist **stack_b)
